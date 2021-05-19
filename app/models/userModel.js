e7b04929eac646c7780e7e9482193bf1124da620
wgermancutraro <german.cutraro@wolox.com.ar>
2021-05-19 14:45:58 -0300

    user migration and model

---
+++ app/models/userModel.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'users',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+        allowNull: false
+      },
+
+      name: { type: DataTypes.STRING },
+      surname: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      surname: { type: DataTypes.STRING, allowNull: false }
+    },
+    {
+      timestamps: false
+    }
+  );
+  return User;
+};
