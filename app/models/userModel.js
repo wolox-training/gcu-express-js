0ac0802fc80b25f515470fa58b147f5c69a5522d
wgermancutraro <german.cutraro@wolox.com.ar>
2021-05-21 17:07:35 -0300

    underscored definition

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       timestamps: true,
+      underscored: true,
       tableName: 'users'
     }
   );
--- migrations/migrations/20210519174318-users-table.js
+++ migrations/migrations/20210519174318-users-table.js
@@ -21,11 +21,11 @@ module.exports = {
         unique: true
       },
       // TIMESTAMPS
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: DataTypes.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: DataTypes.DATE
       }
