4ba4622bd7acd678f210035b70173d49f3ee11c7
wgermancutraro <german.cutraro@wolox.com.ar>
2021-05-25 15:57:43 -0300

    migration removed

--- migrations/migrations/20210525154905-rename-name-column.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  up: queryInterface => queryInterface.renameColumn('tableName', 'name', 'first_name'),
-
-  down: queryInterface => queryInterface.renameColumn('tableName', 'first_name', 'name')
-};
