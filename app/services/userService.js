d4b3dc37b751d27ce9d9f22e687f9b7302371c42
wgermancutraro <german.cutraro@wolox.com.ar>
2021-05-25 12:33:13 -0300

    removed jsdoc

--- app/services/userService.js
+++ app/services/userService.js
@@ -4,39 +4,16 @@ const UserModel = require('../models').user;
 const logger = require('../logger');
 const filterEmail = require('../utils/filterEmail');
 
-/**
- *
- * @param {string} password password
- * @returns {Promise} hashed password
- */
 const encryptPassword = async password => {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   return hash;
 };
 
-/**
- *
- * @param {number} userId id
- * @returns {string} JWT token
- */
 const generateToken = userId => jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30m' });
 
-/**
- *
- * @param {string} email email
- * @returns {Promise} User object
- */
 exports.findUserByEmail = email => UserModel.findOne({ where: { email: filterEmail(email) } });
 
-/**
- *
- * @param {string} name name
- * @param {string} lastName last_name
- * @param {string} email email
- * @param {string} password password
- * @returns {object} User object
- */
 exports.createUser = async ({ name, lastName, email, password }) => {
   const hash = await encryptPassword(password);
 
