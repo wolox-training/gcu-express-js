5cfd441b6f0b2068fcf40eab6d8e9225fdab8ab1
wgermancutraro <german.cutraro@wolox.com.ar>
2021-05-28 17:49:12 -0300

    protected route

---
+++ app/middlewares/protectedRoute.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer')) return res.sendStatus(401);
+
+    const token = authHeader.split('Bearer ')[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    if (!payload.id) return res.sendStatus(401);
+
+    req.user = payload;
+  } catch (err) {
+    return res.sendStatus(401);
+  }
+
+  return next();
+};
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -3,15 +3,17 @@ const router = require('express').Router();
 const userInteractor = require('../interactors/userInteractor');
 const asyncWrapper = require('../utils/asyncWrapper');
 const validateSchema = require('../middlewares/validateSchema');
+const protectedRoute = require('../middlewares/protectedRoute');
 const { userSchema, sessionSchema } = require('../validations');
 const userMapper = require('../mappers/userMapper');
 const httpCodes = require('../constants/httpCodes');
 
 router.get(
   '/',
+  [protectedRoute],
   asyncWrapper(async (req, res) => {
     const { results, pagination } = await userInteractor.getUsers(req.query);
-    return res.status(httpCodes.CREATED).json({ users: results.map(u => userMapper(u)), pagination });
+    return res.status(httpCodes.OK).json({ users: results.map(u => userMapper(u)), pagination });
   })
 );
 
