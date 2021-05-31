006c536242aa11280cdb406c75c8a1d59d7f8291
wgermancutraro <german.cutraro@wolox.com.ar>
2021-05-31 10:18:08 -0300

    user mapper refactor

--- app/mappers/userMapper.js
+++ app/mappers/userMapper.js
@@ -1,20 +1,9 @@
-/**
- * This is the user response.
- * Added in order to avoid return password as response.
- */
-class UserResponse {
-  constructor({ id, firstName, lastName, email, createdAt, updatedAt } = {}) {
-    this.id = id;
-    this.first_name = firstName;
-    this.last_name = lastName;
-    this.email = email;
-    this.created_at = createdAt;
-    this.updated_at = updatedAt;
-  }
-}
-
-const UserMapper = function UserMapper(userDoc) {
-  return new UserResponse(userDoc);
-};
-
-module.exports = UserMapper;
+module.exports = user => ({
+  id: user.id,
+  firstName: user.firstName,
+  lastName: user.lastName,
+  email: user.email,
+  createdAt: user.createdAt,
+  updatedAt: user.updatedAt,
+  role: user.role
+});
