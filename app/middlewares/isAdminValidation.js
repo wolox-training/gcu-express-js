6956448a2efec9ccfd6ed34b5c9761ff9c6fa955
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-03 12:02:35 -0300

    403 status for admin auth

--- app/middlewares/isAdminValidation.js
+++ app/middlewares/isAdminValidation.js
@@ -3,7 +3,7 @@ const errorMessages = require('../constants/errorMessages');
 
 module.exports = (req, res, next) => {
   if (!req.user.id) return next(authenticationError(errorMessages.userNotFound));
-  else if (req.user.role !== 'admin') return res.sendStatus(401);
+  else if (req.user.role !== 'admin') return res.sendStatus(403);
 
   return next();
 };
