1f1c32ccca846311ad41fdf17b39564f447de541
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-04 18:06:52 -0300

    not found error

--- app/interactors/weetInteractor.js
+++ app/interactors/weetInteractor.js
@@ -1,12 +1,12 @@
 const weetService = require('../services/weetsService');
 const userService = require('../services/userService');
 const errorMessages = require('../constants/errorMessages');
-const { validationError, databaseError } = require('../errors');
+const { validationError, notFoundError } = require('../errors');
 const logger = require('../logger');
 
 exports.createWeet = async body => {
   const user = await userService.findUserByEmail(body.email);
-  if (!user) throw databaseError(errorMessages.userNotFound);
+  if (!user) throw notFoundError(errorMessages.userNotFound);
 
   const content = await weetService.getOneRandomPhrase();
   if (content.length > 140) {
