351db3b71358e4a39e16d40dae6e84af282950f8
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-04 18:27:31 -0300

    generate another phrase if it was to long

--- app/interactors/weetInteractor.js
+++ app/interactors/weetInteractor.js
@@ -1,18 +1,13 @@
 const weetService = require('../services/weetsService');
 const userService = require('../services/userService');
 const errorMessages = require('../constants/errorMessages');
-const { validationError, notFoundError } = require('../errors');
-const logger = require('../logger');
+const { notFoundError } = require('../errors');
 
 exports.createWeet = async body => {
   const user = await userService.findUserByEmail(body.email);
   if (!user) throw notFoundError(errorMessages.userNotFound);
 
   const content = await weetService.getOneRandomPhrase();
-  if (content.length > 140) {
-    logger.info('Weet length error');
-    throw validationError(errorMessages.invalidWeet);
-  }
 
   const weet = await weetService.create({ ...body, content });
 
