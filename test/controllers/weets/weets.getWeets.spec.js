5a27bc64bdabf24d9f04856b14cf48d3d8c114c7
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-07 15:41:53 -0300

    factory weets test

--- test/controllers/weets/weets.getWeets.spec.js
+++ test/controllers/weets/weets.getWeets.spec.js
@@ -1,33 +1,31 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const { factory } = require('factory-girl');
+const UserModel = require('../../../app/models').user;
+const WeetModel = require('../../../app/models').weet;
 const app = require('../../../app');
 
+const verify = jest.spyOn(jwt, 'verify');
+factory.define('user', UserModel, {});
+factory.define('weet', WeetModel, { content: 'Lorem Ipsum', userId: factory.assoc('user', 'id') });
+
 describe('GET /weets', () => {
-  let jwtToken = null;
-
   beforeEach(async done => {
-    await request(app)
-      .post('/users/signup')
-      .send({
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@wolox.com.ar',
-        password: 'Sherman33'
-      });
+    verify.mockImplementationOnce(() => ({ id: 1, role: 'admin', email: 'johndoe@wolox.com.ar' }));
+    const userCreated = await factory.create('user', {
+      id: 1,
+      firstName: 'Sherman',
+      lastName: 'Cutraro',
+      password: 'Sherman33',
+      email: 'johndoe@wolox.com.ar',
+      role: 'user'
+    });
+    await factory.createMany('weet', 2, { userId: userCreated.id });
+    done();
+  });
 
-    const loginRequest = await request(app)
-      .post('/users/sessions')
-      .send({
-        email: 'johndoe@wolox.com.ar',
-        password: 'Sherman33'
-      });
-    jwtToken = loginRequest.body.token;
-    await request(app)
-      .post('/weets')
-      .set('Authorization', `Bearer ${jwtToken}`);
-    await request(app)
-      .post('/weets')
-      .set('Authorization', `Bearer ${jwtToken}`);
-
+  afterAll(done => {
+    factory.cleanUp();
     done();
   });
 
@@ -40,7 +38,7 @@ describe('GET /weets', () => {
   it('Should return a list of weets', async () => {
     const { statusCode, body } = await request(app)
       .get('/weets')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.weets).toBeDefined();
@@ -51,7 +49,7 @@ describe('GET /weets', () => {
   it('Should return the first page with a limit of one', async () => {
     const { statusCode, body } = await request(app)
       .get('/weets?limit=1')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.weets).toBeDefined();
@@ -64,7 +62,7 @@ describe('GET /weets', () => {
   it('Should return the second page with a limit of one', async () => {
     const { statusCode, body } = await request(app)
       .get('/weets?limit=1&page=2')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.weets).toBeDefined();
@@ -77,7 +75,7 @@ describe('GET /weets', () => {
   it('Should return all weets with a limit of two and without a page', async () => {
     const { statusCode, body } = await request(app)
       .get('/weets?limit=2')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.weets).toBeDefined();
@@ -89,7 +87,7 @@ describe('GET /weets', () => {
   it('Should return zero results on page 3', async () => {
     const { statusCode, body } = await request(app)
       .get('/weets?limit=1&page=3')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.weets).toBeDefined();
