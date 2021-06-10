93fb40a8839fdbb081facaf4b296dee4b6cd2b25
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-10 16:51:23 -0300

    nodemailer setup

--- app/utils/sendEmail.js
+++ app/utils/sendEmail.js
@@ -6,13 +6,14 @@ const {
 } = require('../../config').common;
 
 const transporter = nodemailer.createTransport({
+  service: 'gmail',
   auth,
   secure: false
 });
 
 const sendEmail = async (to, subject, body) => {
   try {
-    const { messageId } = await transporter.sendEmail({
+    const { messageId } = await transporter.sendMail({
       from: 'noreply@wolox.com.ar',
       to,
       subject,
