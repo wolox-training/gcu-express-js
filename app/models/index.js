87f35b10bdc6502320a70e949e846e8154f66799
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-14 15:48:01 -0300

    add ssl to db config

--- app/models/index.js
+++ app/models/index.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-// const config = require('../../config');
-// const dbConfig = require('../../config/db')[config.environment];
+const config = require('../../config');
+const dbConfig = require('../../config/db')[config.environment];
 
 const basename = path.basename(__filename);
 const db = {};
-// console.log(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: { rejectUnauthorized: false }
-  }
-});
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== basename && file.endsWith('Model.js'))
