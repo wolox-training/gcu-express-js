a2757f6cb3c1550bf7419fa02a67907c164f721a
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-14 15:12:19 -0300

    log db url

--- app/models/index.js
+++ app/models/index.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const config = require('../../config');
-const dbConfig = require('../../config/db')[config.environment];
+// const config = require('../../config');
+// const dbConfig = require('../../config/db')[config.environment];
 
 const basename = path.basename(__filename);
 const db = {};
-console.log(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
-
-const sequelize = new Sequelize(dbConfig.database, {
+// console.log(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+console.log(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  ssl: { rejectUnauthorized: false }
+});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   ssl: { rejectUnauthorized: false }
 });
