1594ad1a45563a5cac95a7f7724cbeb6a08fbca0
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-16 12:22:23 -0300

    auth0 check token middleware

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -3,15 +3,15 @@ const router = require('express').Router();
 const userInteractor = require('../interactors/userInteractor');
 const asyncWrapper = require('../utils/asyncWrapper');
 const validateSchema = require('../middlewares/validateSchema');
-const protectedRoute = require('../middlewares/protectedRoute');
 const isAdminValidation = require('../middlewares/isAdminValidation');
 const { userSchema } = require('../validations');
 const userMapper = require('../mappers/userMapper');
 const httpCodes = require('../constants/httpCodes');
+const checkJwt = require('../middlewares/checkJwt');
 
 router.post(
   '/users',
-  [validateSchema(userSchema), protectedRoute, isAdminValidation],
+  [validateSchema(userSchema), checkJwt, isAdminValidation],
   asyncWrapper(async (req, res) => {
     const { user, token } = await userInteractor.createUserAdmin(req.body);
     return res.status(token ? httpCodes.CREATED : httpCodes.OK).json({ user: userMapper(user), token });
