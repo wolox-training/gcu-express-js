a4774d704b486249e4fb8c32fe3ade2a13a25ee0
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-16 14:24:01 -0300

    getAllUsers test fixed

--- test/controllers/users/users.getAllUsers.spec.js
+++ test/controllers/users/users.getAllUsers.spec.js
@@ -1,46 +1,51 @@
 const request = require('supertest');
+const { factory } = require('factory-girl');
+// const jwksRsa = require('jwks-rsa');
 const app = require('../../../app');
+const UserModel = require('../../../app/models').user;
+// const checkAuth = require('../../../app/middlewares/checkJwt');
+
+// const verify = jest.spyOn(jwksRsa, 'expressJwtSecret');
+factory.define('user', UserModel, {
+  firstName: 'Sherman',
+  lastName: 'Cutraro',
+  password: 'Sherman33',
+  role: 'admin'
+});
+
+jest.mock('../../../app/middlewares/checkJwt.js', () => (req, res, next) => {
+  if (!req.headers.authorization) return res.sendStatus(401);
+  req.user = { id: 1, role: 'admin', email: 'johndoe@wolox.com.ar' };
+  return next();
+});
 
 describe('GET /users', () => {
-  let jwtToken = null;
-
   beforeEach(async done => {
-    await request(app)
-      .post('/users/signup')
-      .send({
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@wolox.com.ar',
-        password: 'Sherman33'
-      });
-    await request(app)
-      .post('/users/signup')
-      .send({
-        firstName: 'Nick',
-        lastName: 'Robinson',
-        email: 'nrobinson@wolox.com.ar',
-        password: 'Sherman33'
-      });
-    const loginRequest = await request(app)
-      .post('/users/sessions')
-      .send({
-        email: 'johndoe@wolox.com.ar',
-        password: 'Sherman33'
-      });
-    jwtToken = loginRequest.body.token;
+    await factory.createMany('user', 2, [
+      {
+        id: 1,
+        email: 'johndoe@wolox.com.ar'
+      },
+      {
+        id: 2,
+        email: 'nick@wolox.com.ar'
+      }
+    ]);
+
     done();
   });
 
-  it('Should return a non authorized error', async () => {
+  it('Should return a non authorized error', async done => {
     await request(app)
       .get('/users')
       .expect(401);
+    done();
   });
 
-  it('Should return a list of users', async () => {
+  it('Should return a list of users', async done => {
     const { statusCode, body } = await request(app)
       .get('/users')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.users).toBeDefined();
@@ -48,12 +53,13 @@ describe('GET /users', () => {
     expect(body.users[0].points).toBeDefined();
     expect(body.users[0].points).toBe('DEVELOPER');
     expect(body.pagination).toBeDefined();
+    done();
   });
 
-  it('Should return the first page with a limit of one', async () => {
+  it('Should return the first page with a limit of one', async done => {
     const { statusCode, body } = await request(app)
       .get('/users?limit=1')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.users).toBeDefined();
@@ -61,12 +67,13 @@ describe('GET /users', () => {
     expect(body.pagination.count).toBe(1);
     expect(body.pagination.limit).toBe(1);
     expect(body.pagination.current_page).toBe(1);
+    done();
   });
 
-  it('Should return the second page with a limit of one', async () => {
+  it('Should return the second page with a limit of one', async done => {
     const { statusCode, body } = await request(app)
       .get('/users?limit=1&page=2')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.users).toBeDefined();
@@ -74,24 +81,26 @@ describe('GET /users', () => {
     expect(body.pagination.count).toBe(1);
     expect(body.pagination.limit).toBe(1);
     expect(body.pagination.current_page).toBe(2);
+    done();
   });
 
-  it('Should return all users with a limit of two and without a page', async () => {
+  it('Should return all users with a limit of two and without a page', async done => {
     const { statusCode, body } = await request(app)
       .get('/users?limit=2')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.users).toBeDefined();
     expect(body.users).toHaveLength(2);
     expect(body.pagination.count).toBe(2);
     expect(body.pagination.limit).toBe(2);
+    done();
   });
 
-  it('Should return zero results on page 3', async () => {
+  it('Should return zero results on page 3', async done => {
     const { statusCode, body } = await request(app)
       .get('/users?limit=1&page=3')
-      .set('Authorization', `Bearer ${jwtToken}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(statusCode).toEqual(200);
     expect(body.users).toBeDefined();
@@ -99,5 +108,6 @@ describe('GET /users', () => {
     expect(body.pagination.count).toBe(0);
     expect(body.pagination.limit).toBe(1);
     expect(body.pagination.current_page).toBe(3);
+    done();
   });
 });
