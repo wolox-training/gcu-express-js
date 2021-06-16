d067f1aefacac3022cc715ad1d86b83d0bf50009
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-16 16:13:30 -0300

    check auth fixed

--- app/middlewares/checkJwt.js
+++ app/middlewares/checkJwt.js
@@ -1,21 +1,13 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
-module.exports = (req, res, next) => {
-  try {
-    const checkJwt = jwt({
-      secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-      }),
-      aud: process.env.AUTH0_AUDIENCE,
-      algorithms: ['RS256']
-    });
-
-    return next(checkJwt);
-  } catch (err) {
-    return res.sendStatus(401);
-  }
-};
+module.exports = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  aud: process.env.AUTH0_AUDIENCE,
+  algorithms: ['RS256']
+});
