20a4b9d3e199415c35c14349cb89bc06e8c420dc
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-16 14:23:50 -0300

    try catch

--- app/middlewares/checkJwt.js
+++ app/middlewares/checkJwt.js
@@ -1,15 +1,21 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
-  aud: process.env.AUTH0_AUDIENCE,
-  algorithms: ['RS256']
-});
+module.exports = (req, res, next) => {
+  try {
+    const checkJwt = jwt({
+      secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+      }),
+      aud: process.env.AUTH0_AUDIENCE,
+      algorithms: ['RS256']
+    });
 
-module.exports = checkJwt;
+    return next(checkJwt);
+  } catch (err) {
+    return res.sendStatus(401);
+  }
+};
