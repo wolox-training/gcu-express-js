866eac999221ecbb8131b0731342cbc55e28b852
wgermancutraro <german.cutraro@wolox.com.ar>
2021-06-16 15:42:57 -0300

    admin & logout test

--- test/controllers/users/users.logout.spec.js
+++ test/controllers/users/users.logout.spec.js
@@ -3,15 +3,18 @@ const { factory } = require('factory-girl');
 const app = require('../../../app');
 const UserModel = require('../../../app/models').user;
 const SessionModel = require('../../../app/models').session;
-const { generateToken } = require('../../../app/services/userService');
+
+jest.mock('../../../app/middlewares/checkJwt.js', () => (req, res, next) => {
+  if (!req.headers.authorization) return res.sendStatus(401);
+  req.user = { id: 1, role: 'admin', email: 'johndoe@wolox.com.ar' };
+  return next();
+});
 
 factory.define('user', UserModel, {});
 
-describe('POST /users/signup', () => {
-  let token = null;
-
+describe('POST /users/logout', () => {
   beforeEach(async () => {
-    const userCreated = await factory.create('user', {
+    await factory.create('user', {
       id: 1,
       firstName: 'Sherman',
       lastName: 'Cutraro',
@@ -19,8 +22,6 @@ describe('POST /users/signup', () => {
       password: 'Sherman33',
       role: 'admin'
     });
-
-    token = generateToken(userCreated);
   });
 
   afterEach(done => {
@@ -29,33 +30,36 @@ describe('POST /users/signup', () => {
     done();
   });
 
-  it('Should return a non authorized error', async () => {
+  it('Should return a non authorized error', async done => {
     await request(app)
       .post('/users/sessions/invalidate_all')
       .expect(401);
+    done();
   });
 
-  it('Should sign out and create a new invalid session', async () => {
+  it('Should sign out and create a new invalid session', async done => {
     const response = await request(app)
       .post('/users/sessions/invalidate_all')
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', 'Bearer abc');
 
     const session = await SessionModel.findOne({ where: { userId: 1 } });
 
     expect(response.statusCode).toBe(200);
     expect(session.userId).toBe(1);
     expect(session.logoutTime).toBeDefined();
+    done();
   });
 
-  it('Should return a 401 error with a expired session token', async () => {
+  it('Should return a 401 error with a expired session token', async done => {
     await request(app)
       .post('/users/sessions/invalidate_all')
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', 'Bearer abc');
 
     const response = await request(app)
       .get('/users')
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', 'Bearer abc');
 
     expect(response.statusCode).toBe(401);
+    done();
   });
 });
